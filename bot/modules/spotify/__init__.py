1379389891232fcffced9958316ede8d71b3c698
BarsTiger <kit.barsic@gmail.com>
2023-10-16 10:21:51 +0300

    Add init to spotify

---
+++ bot/modules/spotify/__init__.py
@@ -0,0 +1,10 @@
+from .spotify import Spotify
+from bot.utils.config import config
+
+
+spotify = Spotify(
+    client_id=config.tokens.spotify.client_id,
+    client_secret=config.tokens.spotify.client_secret
+)
+
+__all__ = ['spotify']
